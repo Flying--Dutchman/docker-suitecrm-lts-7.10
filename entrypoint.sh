ee0499120e2bf1139cff21493e2ad569e22c07c3
Flying--Dutchman <github@dutches.de>
2020-03-20 18:35:19 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,28 +1,78 @@
-#!/bin/bash
-#set -e
+FROM php:7.3-apache
+ARG SUITECRM_VERSION=7.10.24
 
-# logfile=/dev/stdout
+COPY entrypoint.sh php.custom.ini /
+#/usr/local/etc/php/conf.d/
 
-# set file permissions
-# printf '%s\n' "Set permissions" > "$logfile"
+RUN apt-get update && apt-get install -y --no-install-recommends cron \
+	git \
+	dos2unix \
+	libc-client-dev \
+	libcurl4-openssl-dev \
+    libfreetype6-dev \
+	libjpeg62-turbo-dev \
+	libkrb5-dev \
+	libldap2-dev \
+    libmcrypt-dev \
+	libpng-dev \
+	libpq-dev \
+	libssl-dev \
+	libxml2-dev \
+	libzip-dev \
+	unzip \
+	zlib1g-dev \
+	gosu \
+	libcap2-bin \
+	&& curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer \
+	&& apt-get clean \
+	&& rm -rf /var/lib/apt/lists/*
 
-echo "Set permissions"
-cd /var/www/html
+RUN docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/ \
+	&& docker-php-ext-configure intl \
+    && docker-php-ext-configure imap --with-kerberos --with-imap-ssl \
+    && docker-php-ext-configure ldap --with-libdir=lib/x86_64-linux-gnu/ \
+    && docker-php-ext-install -j$(nproc) fileinfo gd imap ldap zip \
+       mysqli pdo_mysql pdo_pgsql soap intl
+	   
+WORKDIR /tmp
 
-echo "Create cache folder if needed..."
-mkdir -p cache 
+#Setting UP SuiteCRM
+RUN gosu www-data curl https://codeload.github.com/salesagility/SuiteCRM/zip/v${SUITECRM_VERSION} -o /tmp/master.zip \
+    && gosu www-data unzip /tmp/master.zip \
+    && gosu www-data mv SuiteCRM-*/* /var/www/html \
+    && rm -rf /tmp/* \
+    && echo "* * * * * cd /var/www/html; php -f cron.php > /dev/null 2>&1 " | crontab -
 
-echo "Set owner of /var/www to www-data..."
-chown -R www-data:www-data /var/www 
+WORKDIR /var/www/html
 
-echo "Set chmod of /var/www recursivly to 755"
-chmod -R 755 /var/www 
-
-echo "Set chmod of cache custom modules themes data upload recursivly to 775"
-chmod -R 775 cache custom modules themes data upload 
-
-echo "Set chmod of config_override.php to 775"
-chmod 775 config_override.php 2>/dev/null
-
-exec "$@"
-
+#Setting Up config file redirect for proper use with docker volumes
+RUN mkdir conf.d \
+    && touch conf.d/config.php \
+    && touch conf.d/config_override.php \
+    && ln -s conf.d/config.php config.php \
+    && ln -s conf.d/config_override.php config_override.php \
+	&& chown -hR www-data:www-data /var/www/html \
+    && gosu www-data composer update --no-dev -n \
+# custom php configurations
+    && mv /php.custom.ini /usr/local/etc/php/conf.d/ \
+# entrypoint
+    && dos2unix /entrypoint.sh \
+    && chmod +x /entrypoint.sh \
+# Change access righs to conf, logs, bin from root to www-data
+    && chown -hR www-data:www-data /etc/apache2/ \
+# setcap to bind to privileged ports as non-root
+    && setcap 'cap_net_bind_service=+ep' /usr/sbin/apache2 \
+    && getcap /usr/sbin/apache2 \
+# cleanup
+    && find /var/www/html -type d -name .git -prune -exec rm -rf {} ';' \
+    && apt remove -y git \
+    && apt autoremove -y \
+    && apt clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+	
+VOLUME /var/www/html/upload
+VOLUME /var/www/html/conf.d
+#ENTRYPOINT ["bash", "/entrypoint.sh"]
+EXPOSE 8080
+HEALTHCHECK --interval=60s --timeout=30s CMD nc -zv localhost 80 || exit 1
+USER www-data
+CMD ["gosu","www-data","apache2-foreground] 
