f18061674be607e858209cd358d5568488dbad4b
Flying--Dutchman <github@dutches.de>
2020-03-21 12:25:19 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,4 +24,5 @@ chmod -R 775 cache custom modules themes data upload
 echo "Set chmod of config_override.php to 775"
 chmod 775 config_override.php 2>/dev/null
 
-exec "$@"
+exec gosu www-data "$@"
+# exec "$@"
